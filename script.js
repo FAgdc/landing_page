0934b51f416eca76baf799fb364bc00b366ce05d
FAgdc <123154251+FAgdc@users.noreply.github.com>
2023-01-25 13:06:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const horizontal_scroll__bar = document.getElementById("horizontal_scroll_brands
 
 var scrollDirection = 1;
 function pageScroll(){
+
+    /*
     if(mobile_MediaQuery.matches){
         
         horizontal_scroll__bar.scrollBy(scrollDirection, 0); // horizontal and vertical scroll increments
@@ -14,9 +16,18 @@ function pageScroll(){
         else if(horizontal_scroll__bar.scrollLeft == 0 && scrollDirection < 0){
             scrollDirection = -1*scrollDirection;
         }
-        /*console.log(horizontal_scroll__bar.scrollLeft);*/
 
-    } 
+    }*/
+
+    horizontal_scroll__bar.scrollBy(scrollDirection, 0);
+    scrolldelay = setTimeout('pageScroll()',20);
+    if(horizontal_scroll__bar.scrollLeft == horizontal_scroll__bar.scrollWidth - horizontal_scroll__bar.clientWidth){
+        scrollDirection = -1*scrollDirection;
+    }
+    else if(horizontal_scroll__bar.scrollLeft == 0 && scrollDirection < 0){
+        scrollDirection = -1*scrollDirection;
+    }
+    /*console.log(horizontal_scroll__bar.scrollLeft);*/
 }
 pageScroll();
 /*------------------------------------------------ AUTOMATED HORIZONTAL SCROLL ------------------------------------------------*/

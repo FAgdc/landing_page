67191a902a5fb1a8b6968d320dbba3042f9f85f5
FAgdc <123154251+FAgdc@users.noreply.github.com>
2023-01-25 13:23:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,30 +4,22 @@ const horizontal_scroll__bar = document.getElementById("horizontal_scroll_brands
 
 var scrollDirection = 1;
 function pageScroll(){
-
-    /*
+    
     if(mobile_MediaQuery.matches){
         
         horizontal_scroll__bar.scrollBy(scrollDirection, 0); // horizontal and vertical scroll increments
         scrolldelay = setTimeout('pageScroll()',20); // scrolls every 50 milliseconds
         if(horizontal_scroll__bar.scrollLeft == horizontal_scroll__bar.scrollWidth - horizontal_scroll__bar.clientWidth){
             scrollDirection = -1*scrollDirection;
+            alert("END LEFT");
         }
         else if(horizontal_scroll__bar.scrollLeft == 0 && scrollDirection < 0){
             scrollDirection = -1*scrollDirection;
+            alert("END RIGHT");
         }
+        /*console.log(horizontal_scroll__bar.scrollLeft);*/
 
-    }*/
-
-    horizontal_scroll__bar.scrollBy(scrollDirection, 0);
-    scrolldelay = setTimeout('pageScroll()',20);
-    if(horizontal_scroll__bar.scrollLeft == horizontal_scroll__bar.scrollWidth - horizontal_scroll__bar.clientWidth){
-        scrollDirection = -1*scrollDirection;
-    }
-    else if(horizontal_scroll__bar.scrollLeft == 0 && scrollDirection < 0){
-        scrollDirection = -1*scrollDirection;
-    }
-    /*console.log(horizontal_scroll__bar.scrollLeft);*/
+    } 
 }
 pageScroll();
 /*------------------------------------------------ AUTOMATED HORIZONTAL SCROLL ------------------------------------------------*/

2679d82756bf3299706be65b56a63bb51b3b6a24
FAgdc <123154251+FAgdc@users.noreply.github.com>
2023-04-09 01:03:36 +0200

    Add files via upload

--- script_v4.js
+++ script_v4.js
@@ -132,9 +132,10 @@ function prev_slide_carousel(){
 
             let carousel_img = document.getElementById("carousel__img__content");
             let carousel_img__array = ["img/palestra_area_cardio.webp", "img/palestra_area_macchine.webp", "img/palestra_area_pesi.webp", "img/palestra_area_strength.webp", "img/palestra_area_esterno.webp"];
-            if(bg_carousel__count <= 0){ bg_carousel__count =  carousel_img__array.length - 1; }
+            if(bg_carousel__count <= 0){ bg_carousel__count = carousel_img__array.length - 1; }
             else{ bg_carousel__count--; }
             carousel_img.src = carousel_img__array[bg_carousel__count];
+            alert(bg_carousel__count);
 
         }catch(error){ alert(error); }
     }
